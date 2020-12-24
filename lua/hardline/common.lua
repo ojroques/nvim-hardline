92d98c42210a9873d648a1cedcf0ef020fd6db84
Olivier Roques <olivier@oroques.dev>
2020-12-24 13:41:30 +0100

    Add dynamic colors

--- lua/hardline/common.lua
+++ lua/hardline/common.lua
@@ -1,5 +1,20 @@
 local M = {}
 
+local modes = {
+  ['?'] = {text = '???', color = 'inactive'},
+  ['n'] = {text = 'NORMAL', color = 'normal'},
+  ['c'] = {text = 'COMMAND', color = 'normal'},
+  ['i'] = {text = 'INSERT', color = 'insert'},
+  ['R'] = {text = 'REPLACE', color = 'replace'},
+  ['v'] = {text = 'VISUAL', color = 'visual'},
+  ['V'] = {text = 'V-LINE', color = 'visual'},
+  [''] = {text = 'V-BLOCK', color = 'visual'},
+  ['s'] = {text = 'SELECT', color = 'visual'},
+  ['S'] = {text = 'S-LINE', color = 'visual'},
+  [''] = {text = 'S-BLOCK', color = 'visual'},
+  ['t'] = {text = 'TERMINAL', color = 'normal'},
+}
+
 function M.echo(hlgroup, msg)
   vim.cmd(string.format('echohl %s', hlgroup))
   vim.cmd(string.format('echo "[hardline] %s"', msg))
@@ -10,8 +25,17 @@ function M.set_active(active)
   vim.api.nvim_win_set_var(0, 'hardline_active', active)
 end
 
-function M.is_active()
+function M.get_active()
   return vim.api.nvim_win_get_var(0, 'hardline_active')
+end
+
+function M.is_active()
+  return M.get_active() == 'active'
+end
+
+function M.get_mode()
+  if not modes[vim.fn.mode()] then return modes['?'] end
+  return modes[vim.fn.mode()]
 end
 
 return M
--- lua/hardline/parts/mode.lua
+++ lua/hardline/parts/mode.lua
@@ -1,47 +1,29 @@
-local M = {}
-local modes = {
-  ['?'] = {text = '???', color = 'inactive'},
-  ['n'] = {text = 'NORMAL', color = 'normal'},
-  ['c'] = {text = 'COMMAND', color = 'normal'},
-  ['i'] = {text = 'INSERT', color = 'insert'},
-  ['R'] = {text = 'REPLACE', color = 'replace'},
-  ['v'] = {text = 'VISUAL', color = 'visual'},
-  ['V'] = {text = 'V-LINE', color = 'visual'},
-  [''] = {text = 'V-BLOCK', color = 'visual'},
-  ['s'] = {text = 'SELECT', color = 'visual'},
-  ['S'] = {text = 'S-LINE', color = 'visual'},
-  [''] = {text = 'S-BLOCK', color = 'visual'},
-  ['t'] = {text = 'TERMINAL', color = 'normal'},
-}
+local common = require('hardline.common')
 
-local function color_item(item)
-  local color = modes[vim.fn.mode()].color
-  return string.format('%%#HardlineMode%s#%s%%##', color, item)
-end
-
-function M.get_mode()
-  if not modes[vim.fn.mode()] then
-    return modes['?'].text
-  end
-  return modes[vim.fn.mode()].text
-end
-
-function M.get_paste()
+local function get_paste()
   if not vim.o.paste then return '' end
-  return '  | PASTE'
+  return '|PASTE'
 end
 
-function M.get_spell()
+local function get_spell()
   if not vim.wo.spell then return '' end
-  return string.format('  | SPELL [%s]', string.upper(vim.bo.spelllang))
+  return string.format('|SPELL[%s]', string.upper(vim.bo.spelllang))
 end
 
-function M.get_item()
-  return color_item(table.concat({
-    [[%{luaeval('require("hardline.parts.mode").get_mode()')}]],
-    [[%{luaeval('require("hardline.parts.mode").get_paste()')}]],
-    [[%{luaeval('require("hardline.parts.mode").get_spell()')}]],
-  }))
+local function get_item()
+  local mode = common.get_mode()
+  return {
+    text = table.concat({
+      ' ',
+      string.format('%s', mode.text),
+      get_paste(),
+      get_spell(),
+      ' '
+    }),
+    mode = mode.color,
+  }
 end
 
-return M
+return {
+  get_item = get_item,
+}

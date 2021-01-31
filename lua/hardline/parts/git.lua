097037a661cfd05c1e542d9097524967070d0cca
Kasfil Aziz <kasf@tuta.io>
2021-01-31 19:08:54 +0700

    fix wrong value in vim-signify empty buffer

--- lua/hardline/parts/git.lua
+++ lua/hardline/parts/git.lua
@@ -7,6 +7,12 @@ local function get_hunks()
     summary = fn.GitGutterGetHunkSummary()
   elseif g.loaded_signify then
     summary = fn['sy#repo#get_stats']()
+    -- signify return [-1,-1,-1] in empty buffer
+    -- that is why we check if one of value is -1
+    -- then stop the operation by return empty string
+    if summary[0] == -1 then
+      return ''
+    end
   else
     -- return empty string if none of them is loaded
     return ''

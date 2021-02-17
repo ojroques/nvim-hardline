5e28db57c9107627780a42be1421fa692a393408
David Zhang <crispgm@gmail.com>
2021-02-17 15:35:05 +0800

    fix: swap cyan and blue for a better look

--- lua/hardline/themes/nord.lua
+++ lua/hardline/themes/nord.lua
@@ -1,7 +1,7 @@
 local colors = {
   black = {gui = '#2E3440', cterm = '235', cterm16 = '0'},
-  blue = {gui = '#5E81AC', cterm = '67', cterm16 = '12'},
-  cyan = { gui = '#88C0D0', cterm = '110', cterm16 = '14'},
+  blue = { gui = '#88C0D0', cterm = '110', cterm16 = '14'},
+  cyan = {gui = '#5E81AC', cterm = '67', cterm16 = '12'},
   green = {gui = '#81A1C1', cterm = '109', cterm16 = '4'},
   grey_comment = {gui = "#4C566A", cterm = "59", cterm16 = "8"},
   grey_cursor = {gui = "#434C5E", cterm = "59", cterm16 = "8"},

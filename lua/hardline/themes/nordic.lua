e24c2311d03e26aa0e0aa8c3536457818d2f0df0
j-guyotbourgeois <62967049+j-guyotbourgeois@users.noreply.github.com>
2021-08-20 09:20:45 +0200

    Nordic theme bufferline edit (#26)
    * Add nordic theme, based on nord colorscheme, similar to the theme on lightline.vim
    
    * edit xterm colors codes
    
    * Edit colors bufferline
    
    * edit background bufferlines colors
    
    Co-authored-by: Jonathan <jonathan@arch.lan>
    Co-authored-by: jonathan <j.guyot-bourgeois@libr.im>

--- lua/hardline/themes/nordic.lua
+++ lua/hardline/themes/nordic.lua
@@ -103,9 +103,9 @@ return {
     separator = inactive,
     current = {
       guifg = colors.black.gui,
-      guibg = colors.green.gui,
+      guibg = colors.cyan.gui,
       ctermfg = colors.black.cterm,
-      ctermbg = colors.green.cterm,
+      ctermbg = colors.cyan.cterm,
     },
     current_modified = {
       guifg = colors.black.gui,
@@ -114,15 +114,15 @@ return {
       ctermbg = colors.cyan.cterm,
     },
     background = {
-      guifg = colors.green.gui,
+      guifg = colors.cyan.gui,
       guibg = colors.black.gui,
-      ctermfg = colors.green.cterm,
+      ctermfg = colors.cyan.cterm,
       ctermbg = colors.black.cterm,
     },
     background_modified = {
-      guifg = colors.blue.gui,
+      guifg = colors.cyan.gui,
       guibg = colors.black.gui,
-      ctermfg = colors.blue.cterm,
+      ctermfg = colors.cyan.cterm,
       ctermbg = colors.black.cterm,
     },
   },

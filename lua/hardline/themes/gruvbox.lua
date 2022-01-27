ab5d98d3100173f6eadef91cc78057d501330bc6
Olivier Roques <oroques@cisco.com>
2022-01-27 19:03:30 +0100

    refactor: prettify

--- lua/hardline/themes/gruvbox.lua
+++ lua/hardline/themes/gruvbox.lua
@@ -1,15 +1,15 @@
 local colors = {
-  black = {gui = "#32302f", cterm = "236", cterm16 = "0"},
-  blue = { gui = "#83a598", cterm = "109", cterm16 = "12"},
-  cyan = {gui = "#8ec07c", cterm = "108", cterm16 = "14"},
-  green = {gui = "#b8bb26", cterm = "142", cterm16 = "10"},
-  grey_comment = {gui = "#928374", cterm = "244", cterm16 = "8"},
-  grey_cursor = {gui = "#665c54", cterm = "241", cterm16 = "8"},
-  grey_menu = {gui = "#7c6f64", cterm = "243", cterm16 = "8"},
-  purple = {gui = "#d3869b", cterm = "175", cterm16 = "13"},
-  red = {gui = "#fb4934", cterm = "167", cterm16 = "9"},
-  white = {gui = "#f2e5bc", cterm = "228", cterm16 = "15"},
-  yellow = {gui = "#fabd2f", cterm = "214", cterm16 = "11"},
+  black = {gui = '#32302f', cterm = '236', cterm16 = '0'},
+  blue = { gui = '#83a598', cterm = '109', cterm16 = '12'},
+  cyan = {gui = '#8ec07c', cterm = '108', cterm16 = '14'},
+  green = {gui = '#b8bb26', cterm = '142', cterm16 = '10'},
+  grey_comment = {gui = '#928374', cterm = '244', cterm16 = '8'},
+  grey_cursor = {gui = '#665c54', cterm = '241', cterm16 = '8'},
+  grey_menu = {gui = '#7c6f64', cterm = '243', cterm16 = '8'},
+  purple = {gui = '#d3869b', cterm = '175', cterm16 = '13'},
+  red = {gui = '#fb4934', cterm = '167', cterm16 = '9'},
+  white = {gui = '#f2e5bc', cterm = '228', cterm16 = '15'},
+  yellow = {gui = '#fabd2f', cterm = '214', cterm16 = '11'},
 }
 
 local inactive = {

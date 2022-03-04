0ee248e2c2f4c9b129eda27a36c78c0c365f22e9
Olivier Roques <oroques@cisco.com>
2022-03-04 14:31:01 +0000

    feat: add support for prompt mode

--- lua/hardline/common.lua
+++ lua/hardline/common.lua
@@ -2,18 +2,19 @@ local fmt = string.format
 local M = {}
 
 M.modes = {
-  ['?'] = {text = '???', state = 'inactive'},
-  ['n'] = {text = 'NORMAL', state = 'normal'},
-  ['i'] = {text = 'INSERT', state = 'insert'},
-  ['R'] = {text = 'REPLACE', state = 'replace'},
-  ['v'] = {text = 'VISUAL', state = 'visual'},
-  ['V'] = {text = 'V-LINE', state = 'visual'},
-  [''] = {text = 'V-BLOCK', state = 'visual'},
-  ['c'] = {text = 'COMMAND', state = 'command'},
-  ['s'] = {text = 'SELECT', state = 'visual'},
-  ['S'] = {text = 'S-LINE', state = 'visual'},
   [''] = {text = 'S-BLOCK', state = 'visual'},
+  [''] = {text = 'V-BLOCK', state = 'visual'},
+  ['?'] = {text = '???', state = 'inactive'},
+  ['R'] = {text = 'REPLACE', state = 'replace'},
+  ['S'] = {text = 'S-LINE', state = 'visual'},
+  ['V'] = {text = 'V-LINE', state = 'visual'},
+  ['c'] = {text = 'COMMAND', state = 'command'},
+  ['i'] = {text = 'INSERT', state = 'insert'},
+  ['n'] = {text = 'NORMAL', state = 'normal'},
+  ['r'] = {text = 'PROMPT', state = 'replace'},
+  ['s'] = {text = 'SELECT', state = 'visual'},
   ['t'] = {text = 'TERMINAL', state = 'command'},
+  ['v'] = {text = 'VISUAL', state = 'visual'},
 }
 
 function M.echo(msg, hlgroup)
